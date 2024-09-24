480a48bcf2049c9e863a098e5e7ba0052bde9402
oscar <opolo@poligran.edu.co>
2024-09-24 18:02:18 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';  // Importa el componente login
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricesComponent } from './prices/prices.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Ruta por defecto
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'features', component: FeaturesComponent },  // Ruta de login
+  { path: 'prices', component: PricesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+];
